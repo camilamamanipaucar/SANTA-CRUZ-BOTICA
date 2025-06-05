22c1f1c40925c15d2b4df4cf982fd288ec9370e4
Camila <paric@kenland>
2025-06-05 05:21:21 -0500

    SEGURIDAD EN LOS MICROSERVE

---
+++ santacruz-auth/src/main/java/com/santacruz/santacruzauth/repositorio/AuthUserRepository.java
@@ -0,0 +1,15 @@
+package com.santacruz.santacruzauth.repositorio;
+
+
+
+import com.santacruz.santacruzauth.Entidad.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface AuthUserRepository extends JpaRepository<AuthUser,Integer> {
+    AuthUser findByUsername(String username);
+
+    Optional<AuthUser> findByUserName(String userName);
+}
