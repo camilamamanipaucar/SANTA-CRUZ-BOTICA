79b4f1fb493c094d76233c08a6e25b141e25b632
Camila <paric@kenland>
2025-06-05 03:32:07 -0500

    mega avanse

--- santacruz-compra/src/main/java/com/santacruz/santacruzcompra/Controlador/CompraDetalleControlador.java
+++ santacruz-compra/src/main/java/com/santacruz/santacruzcompra/Controlador/CompraDetalleControlador.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/compradetalle")
+@RequestMapping("/compradetalles")
 public class CompraDetalleControlador {
     @Autowired
     CompraDetalleServicio servicio;
--- santacruz-venta/src/main/java/com/santacruz/santacruzventa/Controlador/MetodoDePagoControlador.java
+++ santacruz-venta/src/main/java/com/santacruz/santacruzventa/Controlador/MetodoDePagoControlador.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/metododepagos")
+@RequestMapping("/mediodepagos")
 public class MetodoDePagoControlador {
     @Autowired
     MetodoDePagoServicio servicio;
